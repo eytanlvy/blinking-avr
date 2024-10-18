e78cdc03efa733fa374bfa1fcebe1bab37fb4072
yael2718 <yael.pajot@proton.me>
2024-10-18 12:07:07 +0200

    question 3

--- main.s
+++ main.s
@@ -1,8 +1,6 @@
-.text 				; on rentre dans la section .text
-					; elle contient le code du programme
-	<instruction 1> ; exemple : ldi r24, 0xff
-	<instruction 2>
+.text
+	sbi 0x04, 1
+	sbi 0x05, 1
 
-loop: 				; on définit ici un label « loop »
-	rjmp loop 		; on « saute » sur le label « loop »
-					; c’est une boucle infinie
+loop:
+	rjmp loop
