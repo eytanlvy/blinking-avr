3f962085fe0e53f54ba5663b4f03e0fe27cf6190
yael2718 <yael.pajot@proton.me>
2024-10-23 12:13:07 +0200

    500ms

--- exo21.s
+++ exo21.s
@@ -4,8 +4,5 @@ loop:
     dec 0x18        ;1 cycle cpu
     brne loop       ;2 cycles cpu si true, 1 cycle cpu si false
                     ;total = 1 + (1+2)*(val_initiale_r24 - 1) + 2 = 3*(val_initiale_r24)
-    sbi 0x04, 5 ; set DDRB5
-    sbi 0x05, 5 ; set PORTB5
-    cbi 0x05, 5 ; clear PORTB5
 end:
     rjmp end 
--- exo24.s
+++ exo24.s
@@ -1,7 +1,7 @@
 .text
     ldi 0x14, 0xff  ;1 cycle
 loop3:; total : (6 + loop2)*X3 + 1
-    ldi 0x13, 0xff  ;1 cycle
+    ldi 0x13, 0x27  ;1 cycle mettre 0x27 pour faire 500ms
     rjmp loop2      ;2 cycles
     dec 0x14        ;1 cycle
     brne loop3      ;2 cycles si true, 1 cycle si false
@@ -18,5 +18,6 @@ loop1:;total : 3*X1 + 1 cycles
     brne loop1      ;2 cycles si true, 1 cycle si false
     rjmp loop2      ;2 cycles
 ;nb cycles total = (6 + ((6 + 3*X1 + 1)*X2 + 1))*X3 + 1 = (6 + ((7 + 3*X1)*X2 + 1))*X3 + 1 = (7 + 7*X2 + 3*X1*X2)*X3 + 1 = 1 + 7*X3 + 7*X2*X3 + 3*X1*X2*X3
+;avec X1 = X2 = 255, et X3 = 39, on a 8 064 886 cycles, soit a peu pres 500ms
 end:
-    rjmp end 
+    rjmp end
